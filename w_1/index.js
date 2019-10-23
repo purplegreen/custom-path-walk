19f5379514284c7aeffa918067ce41b0e879a7fc
dehlix <deborahligorio@gmail.com>
2019-10-23 21:34:39 +0200

    less is more

--- w_1/index.js
+++ w_1/index.js
@@ -4,9 +4,9 @@ Visitor = class {
         this.code = code
         this.compose = []
     }
-    compose(walkpath) {
+    hasComposed(walkpath) {
         this.walkpath = walkpath
-        this.compose.push(walkpath)
+        walkpath.compose.push(this)
         console.log(`nice path you composed ${this.name}`);
     }
     printWalkPath() {
@@ -16,15 +16,19 @@ Visitor = class {
 
 
 WalkPath = class {
-    contructor(name) {
+    constructor(name) {
         this.name = name
         this.composition = []  
     } 
-    gets(slot) {
-     this.slot = slot
-     this.composition.push(slot) 
-      console.log(`a new slot was added to the guided walk: ${this.name}`)
-   } 
+    getsSlot(slot) {
+        this.slot = slot
+        slot.compose.push(slot)
+         console.log(`a new slot was added to the guided walk: ${this.name}`)
+      } 
+   message() {
+    console.log(`your walk begins with ${this.name[0]} `)
+   }
+   
 }
 
 
@@ -34,14 +38,16 @@ Slot = class {
     this.tipology = tipology
     this.category = category
     this.flow = flow
-    this.duration = duration    
+    this.duration = duration
+    this.walkpaths = []  
+    }
+        isAddedTo(walkpath) {
+            this.walkpaths.push(walkpath)
+            walkpath.composition.push(this) 
+            console.log(`I'm ${this.name} a new slot in the ${this.walpath} walkpath`)
+            }
      }
-     isadded(slot) {
-         this.slot = slot
-         slot.composition.push(this) 
-         console.log(`I'm ${this.name} a new slot in your compositon`)
-     }
- }
+ 
 
 
 // Visitors
@@ -52,13 +58,14 @@ const pino = new Visitor ("Pino", "a003")
 
 // Slots
 const biotope = new Slot ('Biotope', 'walk', 'landscape observation', 'invigorating', '15 min')
-const quarzsand = new Slot ('Quarz Sand', 'seat', 'introspection', 'relaxing', '7 min' )
-const moonlandscape = new Slot ('Moon Landscape', 'walk', 'landscape observation', 'relaxing', '17 min')
+const quarzSand = new Slot ('Quarz Sand', 'seat', 'introspection', 'relaxing', '7 min' )
+const moonScape = new Slot ('Moon Landscape', 'walk', 'landscape observation', 'relaxing', '17 min')
 
 
 // Walk Paths
-const relaxingone = new WalkPath('realaxing walk', ['biotope', 'quarzsand', 'moonlandscape'])
-const longone = new WalkPath('Long Walk', [])
+const relaxingOne = new WalkPath('realaxing walk')
+const longOne = new WalkPath('Long Walk')
 
-longone.gets(moonlandscape)
-biotope.isadded(relaxingone)
+
+biotope.isAddedTo(relaxingOne)
+mina.hasComposed(longOne)
