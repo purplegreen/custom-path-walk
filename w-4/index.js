5990bbd32c58454283f9ec839d5bf1de04457ec9
dehlix <deborahligorio@gmail.com>
2019-11-10 16:17:31 +0100

    all aperations

--- w-4/index.js
+++ w-4/index.js
@@ -30,15 +30,51 @@ app.post("/walkpath", async (req, res) => {
   res.send(walk);
 });
 
-//slots all
+app.delete("/walkpath/:id", async (req, res) => {
+  const walk = await WalkPathService.del(req.params.id);
+  res.send("done");
+});
+
+//slots
 app.get("/slots/all", async (req, res) => {
   const slot = await SlotService.findAll();
   res.render("slots", { slot: slot });
 });
 
+app.get("/slots/:id", async (req, res) => {
+  const slot = await SlotService.find(req.body);
+  res.send(slot);
+});
+
+app.post("/slots", async (req, res) => {
+  const slot = await SlotService.add(req.body);
+  res.send(slot);
+});
+
+app.delete("/slots/:id", async (req, res) => {
+  const slot = await SlotService.del(req.params.id);
+  res.send("ok");
+});
+
+// visitors
 app.get("/visitors/all", async (req, res) => {
   const visitor = await VisitorService.findAll();
   res.render("visitors", { visitor });
+});
+
+app.get("/visitors/:id", async (req, res) => {
+  const visitor = await VisitoService.find(req.body);
+  res.send(visitor);
+});
+
+app.post("/visitors", async (req, res) => {
+  const visitor = await VisitorService.add(req.body);
+  res.send(visitor);
+});
+
+app.delete("/visitors/:id", async (req, res) => {
+  const visitor = await VisitorService.del(req.params.id);
+  res.send("done");
 });
 
 app.listen(3000, () => {
