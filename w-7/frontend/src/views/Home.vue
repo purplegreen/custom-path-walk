9ae6b5b4c2be107b6c019c3f4b322a5e9d129031
dehlix <deborahligorio@gmail.com>
2019-11-29 10:42:53 +0100

    card

--- w-7/frontend/src/views/Home.vue
+++ w-7/frontend/src/views/Home.vue
@@ -11,7 +11,7 @@ export default {
     ...mapState(["walkpaths", "counter"])
   },
   methods: {
-    ...mapActions(["fetchWalkpaths"])
+    ...mapActions(["fetchWalkpaths", "incrementCounter"])
   },
   created() {
     this.fetchWalkpaths();
@@ -20,10 +20,39 @@ export default {
 </script>
 
 <template lang="pug">
-  h1 hello
-   h2 ok 
-   ul 
-    li 
-      h5 {{ walkpaths }}
-      walkpath-card(v-for='walkpath in walkpaths', :walkpath='walkpath')
+  section 
+    button.increment-button(@click="incrementCounter") Increment
+    div {{ counter }}
+    walkpath-card.single-card(v-for='walkpath in walkpaths', :walkpath='walkpath', :key="walkpath.id")
 </template>
+
+
+<style scoped>
+section {
+  padding: 40px 0;
+  border: 1px solid black;
+}
+
+.increment-button {
+  color: darkcyan;
+  padding: 10px 20px;
+  border-radius: 8px;
+  background-color: white;
+  border: 1px solid darkcyan;
+  font-size: 14px;
+  font-weight: 600;
+  cursor: pointer;
+  margin-bottom: 20px;
+}
+
+.increment-button:hover {
+  background-color: darkcyan;
+  color: white;
+}
+
+.single-card {
+  border: 1px solid blue;
+  padding: 20px;
+  margin: 20px;
+}
+</style>
