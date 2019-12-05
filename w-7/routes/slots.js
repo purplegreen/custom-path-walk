677c468c133c7ed083553ea738e5528867eab568
dehlix <deborahligorio@gmail.com>
2019-12-05 10:40:28 +0100

    routes slots fix

--- w-7/routes/slots.js
+++ w-7/routes/slots.js
@@ -9,8 +9,19 @@ router.get("/all", async (req, res) => {
   res.render("list", { items: slots });
 });
 
+router.get("/all/json", async (req, res) => {
+  const slots = await SlotService.findAll();
+  res.send(slots);
+});
+
 router.get("/:id", async (req, res) => {
-  const slot = await SlotService.find(req.body);
+  const slot = await SlotService.find(req.params.id);
+  res.render(slot);
+});
+
+router.get("/:id/json", async (req, res) => {
+  const slot = await SlotService.find(req.params.id);
+  if (!slot) res.status(404);
   res.send(slot);
 });
 
@@ -21,15 +32,14 @@ router.post("/", async (req, res) => {
 
 router.delete("/:id", async (req, res) => {
   const slot = await SlotService.del(req.params.id);
-  res.send("slot");
+  res.send(slot);
 });
 
 router.post("/:id/componetOf", async (req, res) => {
   const slot = await SlotService.find(req.params.id);
   const walkpath = await WalkPathService.find(req.body.walkpath);
   await WalkPathService.composed(slot, walkpath);
-
-  res.send("walkpath");
+  res.send("slot");
 });
 
 module.exports = router;
