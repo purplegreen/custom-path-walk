167bdda701c49ba188cb38983eda7e99f9e43a2d
dehlix <deborahligorio@gmail.com>
2019-12-09 18:02:36 +0100

    app api url

--- w-8-deploy/frontend/src/store/index.js
+++ w-8-deploy/frontend/src/store/index.js
@@ -19,8 +19,20 @@ export default new Vuex.Store({
     SET_WALKPATHS(state, data) {
       state.walkpaths = data;
     },
+    SET_WALKPATH(state, data) {
+      state.walkpath = data;
+    },
     SET_VISITORS(state, data) {
       state.visitors = data;
+    },
+    SET_VISITOR(state, data) {
+      state.visitor = data;
+    },
+    SET_SLOTS(state, data) {
+      state.slots = data;
+    },
+    SET_SLOT(state, data) {
+      state.slot = data;
     }
     // ,
     // SET_POPULATE(state, data) {
@@ -33,12 +45,43 @@ export default new Vuex.Store({
       commit("SET_COUNTER", newCount);
     },
     async fetchWalkpaths({ commit }) {
-      const result = await axios.get("http://localhost:3000/walkpath/all/json");
+      const result = await axios.get(
+        `${process.env.VUE_APP_API_URL}/walkpath/all/json`
+      );
       commit("SET_WALKPATHS", result.data);
     },
+
+    async fetchWalkpath({ commit }, id) {
+      const result = await axios.get(
+        `${process.env.VUE_APP_API_URL}/walkpath/${id}/json`
+      );
+      commit("SET_WALKPATHS", result.data);
+    },
+
     async fetchVisitors({ commit }) {
-      const result = await axios.get("http://localhost:3000/visitors/all/json");
+      const result = await axios.get(
+        `${process.env.VUE_APP_API_URL}/visitors/all/json`
+      );
       commit("SET_VISITORS", result.data);
+    },
+
+    async fetchVisitor({ commit }, id) {
+      const result = await axios.get(
+        `${process.env.VUE_APP_API_URL}/visitors/${id}/json`
+      );
+      commit("SET_VISITORS", result.data);
+    },
+    async fetchSlots({ commit }) {
+      const result = await axios.get(
+        `${process.env.VUE_APP_API_URL}/slots/all/json`
+      );
+      commit("SET_SLOTS", result.data);
+    },
+    async fetchSlot({ commit }, id) {
+      const result = await axios.get(
+        `${process.env.VUE_APP_API_URL}/slot/a${id}/json`
+      );
+      commit("SET_SLOTS", result.data);
     }
     // ,
     // async populateDatabase({ commit }) {
